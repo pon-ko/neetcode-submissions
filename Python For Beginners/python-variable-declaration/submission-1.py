cfc7c76624d8c9e3cb492ae4be162224d011995a
wei jie <246711359+pon-ko@users.noreply.github.com>
2026-07-19 17:02:34 +0800

    Add: python-variable-declaration - submission-1

---
+++ Python For Beginners/python-variable-declaration/submission-1.py
@@ -0,0 +1,7 @@
+message = "this string is stored in a variable"
+# don't modify the code below this line
+#print(message)
+#print(message)
+
+# Alternative (Being Cheeky)
+for i in range(2): print(message)
